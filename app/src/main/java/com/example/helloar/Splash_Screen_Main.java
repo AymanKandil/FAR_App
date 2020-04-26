29d098a8c5b618737714d89f42d10db50047a761
AymanKandil <S00038525@auk.edu.kw>
2020-04-27 00:42:01 +0300

    Splash Screen Files

---
+++ app/src/main/java/com/example/helloar/Splash_Screen_Main.java
@@ -0,0 +1,14 @@
+package com.example.helloar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_Screen_Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen__main);
+    }
+}
