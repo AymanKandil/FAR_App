51c3ebd16f5c9b63adc3e2bac43843dca1413fda
AymanKandil <S00038525@auk.edu.kw>
2020-05-20 13:53:11 +0300

    modified

--- app/src/main/java/com/example/helloar/Splash_Screen_Main.java
+++ app/src/main/java/com/example/helloar/Splash_Screen_Main.java
@@ -40,7 +40,7 @@ public class Splash_Screen_Main extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
                 startActivity(intent);
                 finish();
             }
